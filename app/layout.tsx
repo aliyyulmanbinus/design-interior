b50e0bd84b2e707a899e6e01d49ce49c1cd342c0
aliyyulmanbinus <aliyyulmanbinus@gmail.com>
2025-04-24 15:59:20 +0700

    Merubah title pada tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "kurniainteriorstudio",
+  title: "Kurnia Interior Studio",
   description: "Interior design studio specializing in sophisticated urban living spaces",
     generator: 'v0.dev'
 }
